db547a8318d72fa36b3ac9058a15869c7b49cd84
waldekmastykarz <waldek@mastykarz.nl>
2023-10-12 08:08:52 +0200

    Downgrades Java to 17

--- app/src/main/java/connector/GraphService.java
+++ app/src/main/java/connector/GraphService.java
@@ -1,20 +1,31 @@
 package connector;
 
+import java.io.IOException;
 import java.util.Properties;
+
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.google.common.graph.GraphBuilder;
+import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
 import com.microsoft.graph.models.Request;
 import com.microsoft.graph.requests.GraphServiceClient;
 
 public class GraphService {
     private static GraphServiceClient<Request> client;
 
-    public static GraphServiceClient<Request> getClient() {
+    public static GraphServiceClient<Request> getClient() throws IOException {
         if (client == null) {
             final Properties properties = new Properties();
-            properties.load(App.class.getResourceAsStream("oAuth.properties"));
+            properties.load(App.class.getResourceAsStream("application.properties"));
 
-            // client = GraphServiceClient.builder()
-            //         .authenticationProvider(new AuthenticationProvider())
-            //         .buildClient();
+            final ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+                    .clientId(properties.getProperty("app:clientId"))
+                    .clientSecret(properties.getProperty("app:clientSecret"))
+                    .tenantId(properties.getProperty("app:tenantId"))
+                    .build();
+            final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(graphUserScopes, _deviceCodeCredential);
+
+            GraphServiceClient.builder().
         }
 
         return client;
